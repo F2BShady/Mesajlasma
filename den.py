21103bec6c90f2231650b66a7fee1a054da3e97e
Shady1 <105567588+F2BShady@users.noreply.github.com>
2023-06-01 21:33:42 +0300

    Update den.py
    panel arayüz düzenllemeleri ve ek butonlar eklendi

--- den.py
+++ den.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import messagebox
 import random
 import winsound
 import datetime
@@ -28,6 +29,10 @@ def play_sound():
         winsound.PlaySound("dosyalar/sound.wav", winsound.SND_FILENAME | winsound.SND_ASYNC)
         sound_playing = True
 
+def show_message():
+    message = message_entry.get()
+    messagebox.showinfo("Mesaj Kutusu", message)
+
 root = tk.Tk()
 root.title("Shady Mesajlaşma")
 root.geometry("850x450")
@@ -35,10 +40,13 @@ root.geometry("850x450")
 # Programın üst kısmındaki başlık çubuğuna ikon ekleme
 root.iconbitmap("dosyalar/kerrhigh_1.ico")
 
-label = tk.Label(root, text="Butona Tıklayın", font=("Arial", 24))
+# Arka plan rengini değiştirme
+root.configure(bg="#f0f0f0")
+
+label = tk.Label(root, text="Butona Tıklayın", font=("Arial", 24), bg="#f0f0f0")
 label.pack(pady=20)
 
-button = tk.Button(root, text="Üret", font=("Arial", 14), command=generate_random_text)
+button = tk.Button(root, text="Üret", font=("Arial", 14), command=generate_random_text, bg="lightblue")
 button.pack(pady=10)
 
 sound_playing = False
@@ -55,5 +63,21 @@ pixel_label.pack(side=tk.LEFT, padx=10, pady=5)
 now = datetime.datetime.now()
 date_label = tk.Label(frame, text=now.strftime("%d.%m.%Y"), font=("Arial", 12), bg="#f0f0f0", fg="gray")
 date_label.pack(side=tk.RIGHT, padx=10, pady=5)
+
+# Diğer butonlar ve işlevleri
+button_frame = tk.Frame(root, bg="#f0f0f0")
+button_frame.pack(pady=10)
+
+message_label = tk.Label(button_frame, text="Mesajınız:", font=("Arial", 12), bg="#f0f0f0")
+message_label.pack(side=tk.LEFT, padx=10)
+
+message_entry = tk.Entry(button_frame, font=("Arial", 12), width=30)
+message_entry.pack(side=tk.LEFT, padx=10)
+
+send_button = tk.Button(button_frame, text="Gönder", font=("Arial", 12), command=show_message)
+send_button.pack(side=tk.LEFT, padx=10)
+
+exit_button = tk.Button(button_frame, text="Çıkış", font=("Arial", 12), command=root.quit)
+exit_button.pack(side=tk.LEFT, padx=10)
 
 root.mainloop()
