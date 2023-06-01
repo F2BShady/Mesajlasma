1496bd6607d884e3569185d0f44302e17df92e76
Shady1 <105567588+F2BShady@users.noreply.github.com>
2023-06-01 21:07:59 +0300

    Update den.py
    buton düzenleme,renk uyumu,icon ekleme gibi bazı eklemeler.

--- den.py
+++ den.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import random
 import winsound
+import datetime
 
 def generate_random_text():
     texts = [
@@ -16,8 +17,7 @@ def generate_random_text():
         "Güzel bir gün olsun!"
     ]
     random_text = random.choice(texts)
-    label.config(text=random_text)
-    play_sound()
+    label.config(text=random_text, font=("Arial", 18), fg="blue")
 
 def play_sound():
     global sound_playing
@@ -32,6 +32,9 @@ root = tk.Tk()
 root.title("Shady Mesajlaşma")
 root.geometry("850x450")
 
+# Programın üst kısmındaki başlık çubuğuna ikon ekleme
+root.iconbitmap("dosyalar/kerrhigh_1.ico")
+
 label = tk.Label(root, text="Butona Tıklayın", font=("Arial", 24))
 label.pack(pady=20)
 
@@ -39,5 +42,18 @@ button = tk.Button(root, text="Üret", font=("Arial", 14), command=generate_rand
 button.pack(pady=10)
 
 sound_playing = False
+
+# Alt bölüm için çerçeve (frame) oluşturma
+frame = tk.Frame(root, bg="#f0f0f0")
+frame.pack(fill=tk.X, side=tk.BOTTOM)
+
+# "Pixel" yazısı
+pixel_label = tk.Label(frame, text="Pixel", font=("Arial", 12), bg="#f0f0f0", fg="gray")
+pixel_label.pack(side=tk.LEFT, padx=10, pady=5)
+
+# Tarih bilgisi
+now = datetime.datetime.now()
+date_label = tk.Label(frame, text=now.strftime("%d.%m.%Y"), font=("Arial", 12), bg="#f0f0f0", fg="gray")
+date_label.pack(side=tk.RIGHT, padx=10, pady=5)
 
 root.mainloop()
